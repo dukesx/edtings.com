03417c6874c54fcd47b81da4b7dceab6b73cb7eb
dukesx <afzaalbci@ymail.com>
2023-01-20 20:58:08 +0500

    fixed
    Signed-off-by: dukesx <afzaalbci@ymail.com>

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -61,7 +61,7 @@ const ABC = () => {
           ...generateGradient({
             color1: "blue",
             color2: "teal",
-            opacity: 0.3,
+            opacity: 0.2,
           }),
         }}
         p={0}
